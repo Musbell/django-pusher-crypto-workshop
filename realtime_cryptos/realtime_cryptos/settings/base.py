25bd4d673c79900fd235de016a4dd0887b173f99
Santiago Basulto <santiago.basulto@gmail.com>
2018-03-24 09:01:36 -0300

    Heroku conf

--- realtime_cryptos/realtime_cryptos/settings/base.py
+++ realtime_cryptos/realtime_cryptos/settings/base.py
@@ -131,3 +131,15 @@ STATICFILES_DIRS = [
 LOGIN_REDIRECT_URL = '/team'
 LOGIN_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
+
+# Move this to proper setting files
+SECRET_KEY = 'Juhdsu712382uanjNSDJ%&/AKSMDKm$$'
+
+PUSHER_APP_ID = '496809'
+PUSHER_API_KEY = '6f4dce495b70f4f53d86'
+PUSHER_SECRET_KEY = '8af64dd8915fda14b02d'
+PUSHER_CLUSTER = 'mt1'
+PUSHER_SSL = True
+
+# Client
+PUSHER_APP_KEY = 'de504dc5763aeef9ff52'
