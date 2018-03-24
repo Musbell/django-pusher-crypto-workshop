f0660bcc801a3e16a00a9d1c7a94ecbf3f22b416
Santiago Basulto <santiago.basulto@gmail.com>
2018-03-24 09:10:13 -0300

    Heroku conf

--- realtime_cryptos/realtime_cryptos/wsgi.py
+++ realtime_cryptos/realtime_cryptos/wsgi.py
@@ -6,11 +6,13 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
+from whitenoise.django import DjangoWhiteNoise
 
 import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtime_cryptos.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "realtime_cryptos.settings.prod")
 
 application = get_wsgi_application()
+application = DjangoWhiteNoise(application)
